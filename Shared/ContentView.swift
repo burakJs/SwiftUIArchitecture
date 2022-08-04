0e3612e4722e748920fd8d38a7b8bc1d889131ae
burakJs <burakimdat13@gmail.com>
2022-08-04 03:28:37 +0300

    feat: add hstack, vstack and zstack

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        sampleStackView()
     }
+    
+    fileprivate func sampleHorizontalView() -> some View {
+        return HStack {
+            Spacer().frame(width: 100, alignment: .center)
+            Text("Burak in SwiftUI")
+                .padding()
+            Text("Burak in SwiftUI2")
+                .padding()
+            Text("Burak in SwiftUI3")
+                .padding()
+            Color.red
+            Spacer().frame(width: 100, alignment: .center)
+            
+        }
+    }
+    
+    fileprivate func sampleVerticalView() -> some View {
+        return VStack {
+            Spacer().frame(width: 100, alignment: .center)
+            Text("Burak in SwiftUI")
+                .padding()
+            Text("Burak in SwiftUI2")
+                .padding()
+            Text("Burak in SwiftUI3")
+                .padding()
+            Color.red
+            Spacer().frame(width: 100, alignment: .center)
+            
+        }
+    }
+    
+    fileprivate func sampleStackView() -> some View {
+        return ZStack(alignment: .bottom) {
+            Color.red
+            Spacer().frame(width: 100, alignment: .center)
+            Text("Burak in SwiftUI")
+                .padding()
+            Text("Burak in SwiftUI2")
+                .padding()
+            Text("Burak in SwiftUI3")
+                .padding()
+            
+            Spacer().frame(width: 100, alignment: .center)
+            
+        }
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
