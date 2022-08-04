125a084242a2ac48200f8a65d3f4d7c3b33a9bd9
burakJs <burakimdat13@gmail.com>
2022-08-04 03:02:59 +0300

    Initial Commit

---
+++ Shared/SwiftUIArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIArchitectureApp.swift
+//  Shared
+//
+//  Created by Sr Burak on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
